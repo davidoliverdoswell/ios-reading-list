2451980aad6b918e1f57fa4832ef9f5afbfc9a9e
David Doswell <davidoliverdoswell@icloud.com>
2018-07-31 13:58:50 -0700

    Set up storyboards, identifiers and outlets

---
+++ Reading List/Reading List/Views/BookTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by David Oliver Doswell on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cellLabel: UILabel!
+    
+    @IBOutlet weak var uncheckedButton: UIButton!
+    
+    @IBAction func uncheckedButton(_ sender: Any) {
+        
+    }
+    
+}
+
